00a1b820e6d5d2a33628f18dc0509ec72efa3549
sashaant <antonenko.qa@gmail.com>
2022-05-17 00:09:14 +0300

    refactor info

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,26 @@ import './App.css';
 
 
 
+
 const Data = {
   "name": "Sasha",
   "surname" : "Antonenko",
+  "age": "29",
   "mobile" : "093-123-45-67",
-  "bio": "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
+  "job" : "SDET",
+  "email" : "antonenko.qa@gmail.com",
+  "info": ["I also write and speak about web development, JavaScript, and technology.","Welcome and thanks for stopping by. I hope you enjoy exploring this site as much as I've enjoyed building it."],
+  "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  "skills": ["SDLC","Agile","Automation","Perfomance","JS/TS","Playwright", "Cypress"],
 }
 
 function App() {
   return (
     <div className="App">
-     <h1> Hello {Data.name}!</h1>
-     <h3> Mobile: {Data.mobile}!</h3>
-      <p> {Data.bio}</p>
+        <h1> Hi everyone, I'm {Data.name}</h1>
+        <h3> I'm {Data.job}!</h3>
+        <button>Test click button</button>
+        <p> {Data.info[1]}</p>
 
     </div>
   );
